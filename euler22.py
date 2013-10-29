8e629bd283216dc8c846a60ab423f487909cfd83
Eric Taseski <arvicz22@gmail.com>
2013-10-28 23:48:31 -0400

    finished solving euler22

--- euler22.py
+++ euler22.py
@@ -1,13 +1,36 @@
 from nltk.tokenize import RegexpTokenizer
+from collections import Counter
 
 infile = open('names.txt', 'r')
 names = infile.read()
 
 tokenizer = RegexpTokenizer(r'\w+')
 # tokens = tokenizer.tokenize(names)
-tokens = tokenizer.tokenize("()asdf asdsd asda asda!")
+tokens = tokenizer.tokenize(names)
+
+tokens.sort()
+
+total = 0
+count = 1
+
+# setup the value dictionary
+values = dict()
+
+for x in xrange(0,26):
+	values[chr(ord('A') + x)] = x + 1
 
 for word in tokens:
-	print word
+	thisRun = 0
+	for x in word:
+		thisRun = thisRun + values[x]
+	thisRun = thisRun * count
+	print word, thisRun
+	total = total + thisRun
+	count = count + 1
+
+print total
+
+# for word in tokens:
+# 	print word
 
 # print filter(lambda word: word not in ',-', tokens)
